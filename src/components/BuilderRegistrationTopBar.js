b587cb4d6dada17aeed20c787e9c683e999e351b
Dinastic <mmantiog@hotmail.gr>
2024-04-01 22:55:27 +0300

    tailwind & header

---
+++ src/components/BuilderRegistrationTopBar.js
@@ -0,0 +1,7 @@
+import './Navigation/BuilderRegistrationTopBar.css';
+
+export const BuilderRegistrationTopBar = () => {
+  return (
+      <p className="p-2.5">Είστε έμπειροι στον σχεδιασμό και την κατασκευή επίπλων και κατασκευών; <a href="/" className="font-bold">Εγγραφείτε εδώ</a></p>
+  );
+}
